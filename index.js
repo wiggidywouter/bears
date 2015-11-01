95fff648fb6a5f60fa27e1c273cfc9d9d0a74568
wiggidywouter <you@example.com>
2015-11-01 19:16:22 +0100

    test new 2

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function Bear (type) {
 }
 
 Bear.prototype.growl = function (says) {
-	return 'The ' + this.type + ' bear says ' + (says || 'grrr');
+	return 'The pppppppppppp' + this.type + ' bear says ' + (says || 'grrr');
 };
